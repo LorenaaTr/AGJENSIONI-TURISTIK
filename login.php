2d2bbaac5eaa09e1bb30d684b831cd9d408a13f5
LorenaaTr <lorenatroshupa@gmail.com>
2023-02-26 02:09:51 +0100

    login.php

--- login.php
+++ login.php
@@ -1,46 +1,38 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css\login.css" type="text/css">
-    <title>Log In Form</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="C:\Users\Admin\Desktop\Projekti-Web\css\login.css">
+    <title>Form Validation</title>
+
 </head>
 <body>
-    <main>
-        <div class="container">
-          <div class="log-in__container">
-            <form id="form" class="form" autocomplete="off">
-              <h2>Kyqu</h2>
-              <div class="form-control">
-                <label for="username">Emri i përdoruesit</label>
-                <input
-                  type="text"
-                  id="username"
-                  placeholder="Shëno emrin tuaj të përdoruesit"
-                />
-                <small>Error message</small>
-              </div>
-              
-              <div class="form-control">
-                <label for="password">Fjalëkalimi</label>
-                <input
-                  type="password"
-                  id="password"
-                  placeholder="Shëno fjalëkalimin"
-                />
-                <small>Error message</small>
-              </div>
-              <div class="buttons">
-                <a href="signUp.html" class="cancel-button">Regjistrohu</a>
-                <button type="submit">Hyr</button>
-              </div>
-            </form>
-          </div>
+    <div class="container"> 
+      <div class ="log-in__container">
+        <form id="form" action="/">
+            <h1>Kyçuni</h1>
+            <div class="form-control">
+                <label for="username" id="ep">Emri i përdoruesit</label>
+                <input id="username" name="username" type="text">
+                <div class="error"></div>
+            </div>
+            <div class="form-control">
+                <label for="password" id="f">Fjalëkalimi</label>
+                <input type="password" name="password" id="password">
+                <div class="error"></div>
+            <button type="submit" id="signUpButton" class="logIn"> <b>Hyni</b> </button>
         </div>
-      </main>
-
+            <p></p>
+            <a href="signUp.php">Dëshironi të regjistroheni?</a>
+        </form>
+        </div>
+    </div>
     <script src="javaScript\logIn.js"></script>
 </body>
 </html>
