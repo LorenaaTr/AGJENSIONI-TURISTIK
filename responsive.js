9169b63aeab43189c3e11df5529f790912d1de3c
RinesaGrabovci8 <93592058+RinesaGrabovci8@users.noreply.github.com>
2023-03-12 12:48:35 +0100

    Add files via upload

---
+++ responsive.js
@@ -0,0 +1,6 @@
+let  mobilemenu = document.getElementById("mobile-menu");
+let menuicon = document.getElementById("faIcon");
+
+menuicon.onclick = function(){
+  mobilemenu.classList.toggle("show");
+}
